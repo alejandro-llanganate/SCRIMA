b58e209dc52089044e516ef2e6feae630a79e337
alejandrollanganate <luis.llanganate@epn.edu.ec>
2021-08-05 23:17:25 -0500

    refactor: delete main activity

--- network_scanner/app/src/main/java/com/example/scrima/ui/LogInActivity.kt
+++ network_scanner/app/src/main/java/com/example/scrima/ui/LogInActivity.kt
@@ -1,12 +1,35 @@
 package com.example.scrima.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.scrima.R
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        onClickToOpenActivity(R.id.btn_main_login, this, LogInActivity::class.java)
+        onClickToOpenActivity(R.id.btn_main_signup, this, SignUpActivity::class.java)
+    }
+
+    fun onClickToOpenActivity(idButton: Int, context: Context, classRef: Class<*>) {
+        findViewById<Button>(idButton).setOnClickListener {
+            val intentExplicito = Intent(
+                context,
+                classRef
+            )
+            startActivity(intentExplicito)
+        }
+    }
+
+    fun openActivityWithoutParams(context: Context, clase: Class<*>) {
+        val intentExplicito = Intent(
+            context,
+            clase
+        )
+        startActivity(intentExplicito)
     }
 }
