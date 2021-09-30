60dfb719938d60a2219224f683bed9494fe3087e
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-09-30 09:24:24 -0500

    feat: add network scanner fragment implementation

---
+++ network_scanner/app/src/main/java/com/example/scrima/ui/ResultsScannerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scrima.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scrima.R
+
+class ResultsScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results_scanner)
+    }
+}
